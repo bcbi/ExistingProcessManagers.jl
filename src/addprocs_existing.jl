ac20826861ac74cf1c08c3c432c29b1a080b6415
Dilum Aluthge <dilum@aluthge.com>
2020-10-15 07:22:33 -0400

    Make a docstring more concise (#15)

--- src/addprocs_existing.jl
+++ src/addprocs_existing.jl
@@ -24,28 +24,19 @@ julia> addprocs_existing(hosts_and_ports; kwargs...)
 ### Example 2
 
 ```julia
-julia> w1 = WorkerConfig()
-WorkerConfig(nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing)
+julia> w1 = WorkerConfig();
 
-julia> w1.host = "127.0.0.1"
-"127.0.0.1"
+julia> w1.host = "127.0.0.1";
 
-julia> w1.port = 9601
-9601
+julia> w1.port = 9601;
 
-julia> w2 = WorkerConfig()
-WorkerConfig(nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing)
+julia> w2 = WorkerConfig();
 
-julia> w2.host = "127.0.0.1"
-"127.0.0.1"
+julia> w2.host = "127.0.0.1";
 
-julia> w2.port = 9602
-9602
+julia> w2.port = 9602;
 
-julia> wconfigs = WorkerConfig[w1, w2]
-2-element Vector{WorkerConfig}:
- WorkerConfig(nothing, "127.0.0.1", 9601, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing)
- WorkerConfig(nothing, "127.0.0.1", 9602, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing, nothing)
+julia> wconfigs = WorkerConfig[w1, w2];
 
 julia> addprocs_existing(wconfigs; kwargs...)
 ```
@@ -59,8 +50,7 @@ julia> worker_output = \"""
        julia_worker:9960#192.168.1.151
        julia_worker:9966#192.168.1.157
        julia_worker:9968#192.168.1.159
-       \"""
-"julia_worker:9960#192.168.1.151\njulia_worker:9960#192.168.1.151\njulia_worker:9960#192.168.1.151\njulia_worker:9966#192.168.1.157\njulia_worker:9968#192.168.1.159\n"
+       \""";
 
 julia> addprocs_existing(worker_output; kwargs...)
 ```
