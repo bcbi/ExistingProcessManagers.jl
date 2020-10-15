8098941d714c58d14190e4a8f41e22fa05297690
Dilum Aluthge <dilum@aluthge.com>
2020-10-15 07:09:25 -0400

    Add an example (#13)

--- docs/make.jl
+++ docs/make.jl
@@ -13,7 +13,7 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
-        "Example" => "example.md",
+        "Examples" => "examples.md",
         "API" => "api.md",
     ],
     strict=true,
